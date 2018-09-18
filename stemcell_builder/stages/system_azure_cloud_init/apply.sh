01e85e9d58288b30de5a26457bdf997ede068633
gossion <guwe@microsoft.com>
2018-09-18 10:10:37 +0000

    ubuntu not able to login

--- stemcell_builder/stages/system_azure_cloud_init/apply.sh
+++ stemcell_builder/stages/system_azure_cloud_init/apply.sh
@@ -11,7 +11,7 @@ then
     apt-get update # check update?
     apt-get install -y cloud-init
   "
-  #cp -f $dir/assets/etc/cloud/cloud.cfg $chroot/etc/cloud/cloud.cfg
+  cp -f $dir/assets/etc/cloud/cloud.cfg $chroot/etc/cloud/cloud.cfg
   #cp -f $dir/assets/etc/cloud/cloud.cfg.d/05_logging.cfg $chroot/etc/cloud/cloud.cfg.d/05_logging.cfg
   #cp -f $dir/assets/etc/cloud/cloud.cfg.d/90-azure.cfg $chroot/etc/cloud/cloud.cfg.d/90-azure.cfg
   #cp -f $dir/assets/etc/cloud/cloud.cfg.d/91_walinuxagent.cfg $chroot/etc/cloud/cloud.cfg.d/91_walinuxagent.cfg
@@ -22,7 +22,7 @@ then
     yum check-update cloud-init -y
     yum install cloud-init -y
   "
-  cp -f $dir/assets/etc/rsyslog.d/21-cloudinit.conf $chroot/etc/rsyslog.d/21-cloudinit.conf
+  #cp -f $dir/assets/etc/rsyslog.d/21-cloudinit.conf $chroot/etc/rsyslog.d/21-cloudinit.conf
 else
   echo "Unknown OS '${os_type}', exiting"
   exit 2
@@ -30,10 +30,10 @@ fi
 
 run_in_chroot $chroot "
    #cloud-init init --local
-   #rm -rf /var/lib/cloud/instances/* 
-   touch /var/log/cloudinit.log
-   chown syslog:syslog /var/log/cloudinit.log
-   chmod 600 /var/log/cloudinit.log
+   #rm -rf /var/lib/cloud/instances/*
+   #touch /var/log/cloudinit.log
+   #chown syslog:syslog /var/log/cloudinit.log
+   #chmod 600 /var/log/cloudinit.log
 "
 #cp -f $dir/assets/var/lib/cloud/scripts/per-once/firstboot.sh $chroot/var/lib/cloud/scripts/per-once/firstboot.sh #Not need to run everytime
 
